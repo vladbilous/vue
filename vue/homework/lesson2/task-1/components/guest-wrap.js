cbf4a79061662bbe621d89cf207d88b48540ea66
Vlad Bilous <vladbilous17@gmail.com>
2021-11-25 19:02:44 +0200

    fix homework2

--- vue/homework/lesson2/task-1/components/guest-wrap.js
+++ vue/homework/lesson2/task-1/components/guest-wrap.js
@@ -1,27 +1,35 @@
-const GuestCard = {
-    name: 'GuestCard',
+const GuestWrap = {
+    name: 'guestWrap',
     props: {
         guest: {
             type: Object,
             required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
         }
     },
-    data() {
-        return {
-            defaultData: ""
-        }
-    },
+    // data() {
+    //     return {
+    //         defaultData: ""
+    //     }
+    // },
+
     template: `
-        <p>
-            <div >
-                    <guest-card 
-                        v-for="guest in guests" 
-                        :key="guest['_id']"
-                        :guest="guest"
-                    />
+        <div class="container">
+            <div class="row">
+            <div v-bind:class="{done: completed}">
+            <input value="check" type="checkbox" 
+                v-on:change="completed = !completed"
+                @change="guest.isPresent = !guest.isPresent" 
+                >
+                    <span>{{ guest.index + 1 }})</span>
+                    <span>{{ guest.name }}</span>
+                </div>
             </div>
+        </div>
             
-        </p>
     `
 
 }
--- vue/homework/lesson2/task-1/main.js
+++ vue/homework/lesson2/task-1/main.js
@@ -1,10 +1,35 @@
 const app = new Vue({
     el: '#app',
     components: {
-        'guest': guestCard,
+        'guest': GuestWrap
     },
     data: {
-        guestCard
+        guestsList,
+        title: 'list users',
+        number: 10
+    },
+    methods: {
+        people(value) {
+            if (value === 'present') {
+                this.number = 10
+                this.guestsList = guestsList.filter(guest => guest.isPresent)
+            } else if (value === 'absent') {
+                this.guestsList = guestsList.filter(guest => !guest.isPresent)
+                this.number = 10
+            } else if (value === 'else') {
+                this.number += 10
+            } else {
+                this.guestsList = guestsList
+                this.number = guestsList.length
+            }
+        }
+    },
+
+    beforeCreate(){
+        for (let i = 0; i < guestsList.length; i++) {
+            guestsList[i]["isPresent"] = false
+        }
     }
+
 
 })
